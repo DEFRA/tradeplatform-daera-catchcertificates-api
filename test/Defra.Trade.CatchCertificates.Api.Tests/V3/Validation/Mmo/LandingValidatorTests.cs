c2879f5ab638c47cf926e0f2f01561f849661155
John Fox <163529520+JohnFoxCap@users.noreply.github.com>
2025-09-19 11:45:36 +0100

    Bugs/617197 eez (#18)
    * AB#617197 Changed type for ExclusiveEconomicZones

--- test/Defra.Trade.CatchCertificates.Api.Tests/V3/Validation/Mmo/LandingValidatorTests.cs
+++ test/Defra.Trade.CatchCertificates.Api.Tests/V3/Validation/Mmo/LandingValidatorTests.cs
@@ -115,7 +115,7 @@ public class LandingValidatorTests
             .WithErrorMessage("'High Seas Area' must not be empty.");
 
         result.ShouldHaveValidationErrorFor(x => x.ExclusiveEconomicZones)
-            .WithErrorMessage("The specified condition was not met for 'Exclusive Economic Zones'.");
+            .WithErrorMessage("'Exclusive Economic Zones' must not be empty.");
 
         result.ShouldHaveValidationErrorFor(x => x.StartDate)
             .WithErrorMessage("'Start Date' must not be empty.");
