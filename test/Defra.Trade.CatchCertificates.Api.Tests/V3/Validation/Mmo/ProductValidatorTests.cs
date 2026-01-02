de044aa0ad30a55ad464875ccc11e74cb17968de
John Fox <163529520+JohnFoxCap@users.noreply.github.com>
2026-01-02 11:38:39 +0000

    Updated products validation (#21)

--- test/Defra.Trade.CatchCertificates.Api.Tests/V3/Validation/Mmo/ProductValidatorTests.cs
+++ test/Defra.Trade.CatchCertificates.Api.Tests/V3/Validation/Mmo/ProductValidatorTests.cs
@@ -72,16 +72,10 @@ public class ProductValidatorTests
         result.ShouldHaveValidationErrorFor(x => x.ExportedWeight)
             .WithErrorMessage("'Exported Weight' must not be empty.");
 
-        result.ShouldHaveValidationErrorFor(x => x.DateOfUnloading)
-            .WithErrorMessage("'Date Of Unloading' must not be empty.");
-
-        result.ShouldHaveValidationErrorFor(x => x.TransportUnloadedFrom)
-            .WithErrorMessage("'Transport Unloaded From' must not be empty.");
-
         result.ShouldHaveValidationErrorFor(x => x.Validation)
             .WithErrorMessage("'Validation' must not be empty.");
 
-        result.Errors.Should().HaveCount(9);
+        result.Errors.Should().HaveCount(7);
 
         Mock.Verify(_productValidationValidator);
     }
